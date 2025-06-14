9f842ba444fa62063946bd39843bdf8defd7f525
André Santos <andreluissilsan@hotmail.com>
2025-06-14 13:06:26 -0300

    feat: to win & challenge migration + goals seeder(premier league at the moment)

--- database/seeders/teams.php
+++ database/seeders/teams.php
@@ -1,10 +1,8 @@
 <?php
 
 namespace Database\Seeders;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class teams extends Seeder
 {
